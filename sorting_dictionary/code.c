08dc4ea7b57ab0fd50a22949eaf54a045ad7a7eb
Diego Aldana <diegaso0704@gmail.com>
2025-05-16 13:07:15 +0900

    Finished C version of sorted dictionary

--- sorting_dictionary/code.c
+++ sorting_dictionary/code.c
@@ -2,25 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <time.h>
 
 typedef struct{
     char* entry;
     char* word;
+    int number;
 }DictionaryEntry;
 
 //swap function
-void swap(int* a, int x, int y){
-    int t = a[x];
+void swap(DictionaryEntry* a, int x, int y){
+    DictionaryEntry t = a[x];
     a[x] = a[y];
     a[y] = t;
 }
 
 //insert sort
-void insert_sort(int* a, int n){
-    int i, j, key;
+void insert_sort(DictionaryEntry* a, int n){
+    int i, j;
+    DictionaryEntry key;
     for(i = 1; i < n; i++){
         key = a[i];
-        for(j = i-1; j >= 0 && a[j] > key; j--){
+        for(j = i-1; j >= 0 && strcmp(a[j].word, key.word) > 0; j--){
             a[j+1] = a[j];
         }
         a[j+1] = key;
@@ -28,13 +31,13 @@ void insert_sort(int* a, int n){
 }
 
 //partitioner of quick sort, returns pivot
-int partitioner(int *a, int start, int end){
-    int pivot = a[end];
+int partitioner(DictionaryEntry *a, int start, int end){
+    DictionaryEntry pivot = a[end];
     int i = start - 1;
     for(int j = start; j<=end; j++){
-        if(a[j] < pivot){
+        if(strcmp(a[j].word,pivot.word) < 0){
             i++;
-            swap(a, a[j], a[i]);
+            swap(a, j, i);
         }
     }
     i++;
@@ -43,23 +46,30 @@ int partitioner(int *a, int start, int end){
 }
 
 //quick sort
-void quick_sort(int* a, int start, int end){
-    if(end <= start) return;
+void quick_sort(DictionaryEntry* a, int start, int end){
+    while (start < end) {
+        int pivot = partitioner(a, start, end);
 
-    int pivot = partitioner(a, start, end);
-    quick_sort(a, start, pivot-1);
-    quick_sort(a, pivot+1, end);
+        if (pivot - start < end - pivot) {
+            quick_sort(a, start, pivot - 1);
+            start = pivot + 1;
+        } else {
+            quick_sort(a, pivot + 1, end);
+            end = pivot - 1;
+        }
+    }
 }
 
 //heapify
-void heapify(int* a, int root, int n){
-    int child, rootkey;
-    int temp;
+void heapify(DictionaryEntry* a, int root, int n){
+    int child;
+    DictionaryEntry temp, rootkey;
     temp = a[root];
+    rootkey = a[root];
     child = 2 * root;
     while(child <= n){
-        if((child < n) && (a[child] < a[child + 1])) child++;
-        if(rootkey > a[child]) break;
+        if((child < n) && (strcmp(a[child].word, a[child + 1].word) < 0)) child++;
+        if(strcmp(rootkey.word, a[child].word) > 0) break;
         else{
             a[child / 2] = a[child];
             child *= 2;
@@ -69,11 +79,11 @@ void heapify(int* a, int root, int n){
 }
 
 //heap sort
-void heapsort(int* a, int n){
+void heapsort(DictionaryEntry* a, int n){
     int i;
     for(i = n / 2; i > 0; i--) heapify(a, i, n);
     for(i = n - 1; i > 0; i--){
-        swap(a, a[1], a[i+1]);
+        swap(a, 1, i+1);
         heapify(a, 1, i);
     }
 }
@@ -95,14 +105,14 @@ void remove_newline(char* str) {
     }
 }
 
-int main(){
+//Initialize dictionary array
+DictionaryEntry* array_initializer(int* currentSize){
     FILE* file = fopen("randdict_utf8.TXT", "r");
 
     char line[256];
     int dictionary_max_size = 20;
     DictionaryEntry* dictionary = malloc(dictionary_max_size * sizeof(DictionaryEntry));
 
-    int currentSize = 0;
     if(file != NULL){
         while(fgets(line,sizeof(line), file)){
             DictionaryEntry current;
@@ -116,17 +126,95 @@ int main(){
             for(int i = 0; i < strlen(word); i++) word[i] = tolower(word[i]);
             strcpy(current.word, word);
 
-            dictionary[currentSize++] = current;
+            dictionary[(*currentSize)++] = current;
 
-            if(currentSize >= dictionary_max_size){
+            if((*currentSize) >= dictionary_max_size){
                 dictionary_max_size *= 2;
                 dictionary = realloc(dictionary, dictionary_max_size * sizeof(DictionaryEntry));
             }
         }
         fclose(file);
     }
+    return dictionary;
+}
 
-    
-    
+//print array
+void print_array(DictionaryEntry* a, int n){
+    for(int i = 0; i < n; i++) printf("Entry: %s Word: %s\n", a[i].entry, a[i].word);
+}
+
+//function time measurer
+double measure_time(int x, DictionaryEntry* a, int n){ 
+    clock_t t;
+    t = clock();
+    switch(x){
+        case 1:
+            insert_sort(a, n);
+            break;
+        case 2:
+            quick_sort(a, 0, n-1);
+            break;
+        case 3:
+            heapsort(a, n-1);
+            break;
+    }
+    t = clock() - t;
+    double time_taken = ((double)t) / CLOCKS_PER_SEC;
+    return time_taken;
+}
+
+//number assigner
+void num_assign(DictionaryEntry* a, int n) {for(int i = 0; i < n; i++) a[i].number = i+1;}
+
+//BINARY SEARCH FUNCTION
+DictionaryEntry search_word(DictionaryEntry* a, int n, char* objective, int* cmp_cnt){
+    for(int i = 0; i < strlen(objective); i++) objective[i] = tolower(objective[i]); //to lower case
+    int middle, start = 0, end = n;
+    while(start <= end){
+        middle = (int) (start+end) / 2;
+        (*cmp_cnt)++;
+        if(strcmp(a[middle].word, objective) == 0) return a[middle]; //return if found
+        else if(strcmp(a[middle].word, objective) > 0) end = middle;
+        else if(strcmp(a[middle].word, objective) < 0) start = middle;
+    }
+    DictionaryEntry notFound = {"Not found", "Undefined", -1};
+    return notFound;
+}
+
+int main(){
+    int size = 0, size2 = 0, size3= 0;
+    DictionaryEntry* dictionary = array_initializer(&size);
+    DictionaryEntry* dictionary2 = array_initializer(&size2);
+    DictionaryEntry* dictionary3 = array_initializer(&size3); 
+
+    //CALCULATE AND SHOW TIME TAKEN
+    double inst_time = measure_time(1, dictionary, size);
+    double quik_time = measure_time(2, dictionary2, size2);
+    double heap_time = measure_time(3, dictionary3, size3);
+
+    printf("Insertion sort time: %fs\n", inst_time);
+    printf("Quick sort time    : %fs\n", quik_time);
+    printf("Heap sort time     : %fs\n", heap_time);
+
+    //SEARCH RELATED CODE
+
+    //assign numbers for words
+    num_assign(dictionary, size);
+
+    //search
+    char objective[256];
+    while(1){
+        //input scan
+        printf("Search for word (-1 to exit): ");
+        scanf("%s", objective);
+
+        //if wants to exit
+        if(strcmp("-1", objective) == 0) break;
+
+        //search for word
+        int comparisson_count = 0;
+        DictionaryEntry result = search_word(dictionary, size-1, objective, &comparisson_count);
+        printf("(%d:%d) %s\n\n", result.number, comparisson_count, result.entry);
+    }
     return 0;
 }
